260373eda492a3abb370429eaa426ab9fa6b2f44
karishmas6 <carishmashukla28@gmail.com>
2021-07-22 23:39:30 +0530

    Component setup

--- src/components/PckgList.tsx
+++ src/components/PckgList.tsx
@@ -1,12 +1,22 @@
+import { useState } from "react";
+
 const PckgList: React.FC = () => {
+    const [pckg, setPckg] = useState('');
+
+    const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+    }
     return (
         <div>
-           <form>
-               <input />
+           <form onSubmit={onSubmit}>
+               <input 
+               value={pckg} 
+               onChange={e => setPckg(e.target.value)} 
+               />
                <button>Search</button>
            </form>
         </div>
     )
 }
 
-export default PckgList
+export default PckgList;

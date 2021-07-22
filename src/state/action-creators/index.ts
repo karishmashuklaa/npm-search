5c7ee6073fd734015fad3fe31b9d74fa1df87e97
karishmas6 <carishmashukla28@gmail.com>
2021-07-22 21:35:35 +0530

    Actions

---
+++ src/state/action-creators/index.ts
@@ -0,0 +1,34 @@
+import axios from "axios";
+import { ActionType } from "../action-types/types";
+import { Action } from "../actions";
+
+const searchPckg = (pckg: string) => async (dispatch: any) => {
+  dispatch({
+    type: ActionType.SEARCH_PCKG,
+  });
+
+  try {
+    const { data } = await axios.get(
+        "https://registry.npmjs.org/-/v1/search", 
+        {
+        params: {
+            text: pckg
+        }
+    }); 
+
+    const names = data.objects.map((result: any) => {
+        return result.package.name;
+    })
+
+    dispatch({
+        type: ActionType.SEARCH_PCKG_SUCCESS,
+        payload: names
+    });
+
+  } catch (err) {
+    dispatch({
+      type: ActionType.SEARCH_PCKG_ERROR,
+      payload: err.message,
+    });
+  }
+};

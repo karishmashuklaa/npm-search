b4f404fefa5cc745f92ebd8e519cd77ce49f7246
karishmas6 <carishmashukla28@gmail.com>
2021-07-22 19:24:47 +0530

    Reducer

--- src/state/reducers/pckgReducer.ts
+++ src/state/reducers/pckgReducer.ts
@@ -1,48 +1,49 @@
 interface PckgState {
-    loading: boolean;
-    error: string | null;
-    data: string[];
+  loading: boolean;
+  error: string | null;
+  data: string[];
 }
 
 interface SearchPckgAction {
-    type: 'search_pckg';
+  type: "search_pckg";
 }
 
 interface SearchPckgSuccessAction {
-    type: 'search_pckg_success';
-    payload: string[];
+  type: "search_pckg_success";
+  payload: string[];
 }
 
 interface SearchPckgErrorAction {
-    type: 'search_pckg_error';
-    payload: string;
+  type: "search_pckg_error";
+  payload: string;
 }
 
-const reducer = (state: PckgState, action): PckgState => {
-    const { type, payload } = action;
-    switch(type) {
-        case 'search_pckg':
-            return {
-                loading: true,
-                error: null,
-                data: []
-            };
-        case 'search_pckg_success':
-            return {
-                loading: false,
-                error: null,
-                data: payload
-            };
-        case 'search_pckg_error':
-            return {
-                loading: false,
-                error: payload,
-                data: []
-            };
-        default:
-            return state;
-    }
-
+const reducer = (
+  state: PckgState,
+  action: SearchPckgAction | SearchPckgSuccessAction | SearchPckgErrorAction
+): PckgState => {
+  switch (action.type) {
+    case "search_pckg":
+      return {
+        loading: true,
+        error: null,
+        data: [],
+      };
+    case "search_pckg_success":
+      return {
+        loading: false,
+        error: null,
+        data: action.payload,
+      };
+    case "search_pckg_error":
+      return {
+        loading: false,
+        error: action.payload,
+        data: [],
+      };
+    default:
+      return state;
+  }
 };
 
 export default reducer;

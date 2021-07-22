a2028b3f683abcaf48369096dd69735ea240997c
karishmas6 <carishmashukla28@gmail.com>
2021-07-22 23:50:35 +0530

    Custom hook

--- src/components/PckgList.tsx
+++ src/components/PckgList.tsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
+import { useActions } from "../hooks/useActions";
 
 const PckgList: React.FC = () => {
     const [pckg, setPckg] = useState('');
-
+    const { searchPckg } = useActions();
     const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
+        searchPckg(pckg);
     }
     return (
         <div>

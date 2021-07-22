7d0123d20a2c818b9ef7b4823bcf7ad334251d33
karishmas6 <carishmashukla28@gmail.com>
2021-07-23 00:18:55 +0530

    Custom hook

--- src/components/PckgList.tsx
+++ src/components/PckgList.tsx
@@ -1,9 +1,12 @@
 import { useState } from "react";
+import { useTypedSelector } from "../hooks/useTypedSelector";
 import { useActions } from "../hooks/useActions";
 
 const PckgList: React.FC = () => {
     const [pckg, setPckg] = useState('');
     const { searchPckg } = useActions();
+    const { data, loading, error } = useTypedSelector((state) => state.pckgs);
+
     const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         searchPckg(pckg);
